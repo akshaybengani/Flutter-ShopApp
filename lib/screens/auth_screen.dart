152c93a300cd74ff2ed1da97c4e7e578feed6f0d
Akshay Bengani <akshaybengani@gmail.com>
2020-02-03 18:26:54 +0530

    AnimatedContainer is great for handling animations without any animator object and without any init state thing The Fade Animation is used to hide or show a widget and it takes a animation object of double generic type which is used for the opacity controller and opacity values range in between 0 and 1 since the fade animation works on the same animation controller we need to cover it up with a AnimatedContainer which takes constraints calculated based on the authmode weather signup or login and min and max heights are defined there

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -111,6 +111,7 @@ class _AuthCardState extends State<AuthCard>
   AnimationController _controller;
   // This is The animation we actually going to perform it is a generic type
   Animation<Size> _heightAnimation;
+  Animation<double> _opacityAnimation;
 
   @override
   void initState() {
@@ -135,10 +136,14 @@ class _AuthCardState extends State<AuthCard>
         curve: Curves.linear,
       ),
     );
+    _opacityAnimation = Tween(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeIn),
+    );
+
     // we have currently manually managed the listener for the animation
-    _heightAnimation.addListener(() {
-      setState(() {});
-    });
+    // _heightAnimation.addListener(() {
+    //   setState(() {});
+    // });
   }
 
   void _showErrorDialog(String message) {
@@ -237,16 +242,17 @@ class _AuthCardState extends State<AuthCard>
         borderRadius: BorderRadius.circular(10.0),
       ),
       elevation: 8.0,
-      child: AnimatedBuilder(
-        animation: _heightAnimation,
-        builder: (ctx, ch) => Container(
-            //height: _authMode == AuthMode.Signup ? 320 : 260,
-            height: _heightAnimation.value.height,
-            constraints:
-                BoxConstraints(minHeight: _heightAnimation.value.height),
-            width: deviceSize.width * 0.75,
-            padding: EdgeInsets.all(16.0),
-            child: ch),
+      // Animated Container is great we dont need to create a sepearte Animation object
+      // no listener no shit in init state all will be done automatocally and thats all
+      child: AnimatedContainer(
+        height: _authMode == AuthMode.Signup ? 320 : 260,
+        duration: Duration(milliseconds: 300),
+        curve: Curves.easeIn,
+        //height: _heightAnimation.value.height,
+        constraints: BoxConstraints(minHeight: _heightAnimation.value.height),
+        width: deviceSize.width * 0.75,
+        padding: EdgeInsets.all(16.0),
+
         child: Form(
           key: _formKey,
           child: SingleChildScrollView(
@@ -277,19 +283,34 @@ class _AuthCardState extends State<AuthCard>
                     _authData['password'] = value;
                   },
                 ),
-                if (_authMode == AuthMode.Signup)
-                  TextFormField(
-                    enabled: _authMode == AuthMode.Signup,
-                    decoration: InputDecoration(labelText: 'Confirm Password'),
-                    obscureText: true,
-                    validator: _authMode == AuthMode.Signup
-                        ? (value) {
-                            if (value != _passwordController.text) {
-                              return 'Passwords do not match!';
-                            }
-                          }
-                        : null,
+                AnimatedContainer(
+                  duration: Duration(milliseconds: 300),
+                  constraints: BoxConstraints(
+                    minHeight: _authMode == AuthMode.Signup ? 60 : 0,
+                    maxHeight: _authMode == AuthMode.Signup ? 120 : 0,
                   ),
+                  curve: Curves.easeIn,
+                  // Fade Transition Animation is used to add or remove a widget with an animation
+                  // so we just deleted the if condition to check and added FadeTransition
+                  // Since the FadeTransition requires a Animation object we need to create a new Animation
+                  // And that Animation is a double value container which takes opacity values between 0 and 1
+                  child: FadeTransition(
+                    opacity: _opacityAnimation,
+                    child: TextFormField(
+                      enabled: _authMode == AuthMode.Signup,
+                      decoration:
+                          InputDecoration(labelText: 'Confirm Password'),
+                      obscureText: true,
+                      validator: _authMode == AuthMode.Signup
+                          ? (value) {
+                              if (value != _passwordController.text) {
+                                return 'Passwords do not match!';
+                              }
+                            }
+                          : null,
+                    ),
+                  ),
+                ),
                 SizedBox(
                   height: 20,
                 ),
